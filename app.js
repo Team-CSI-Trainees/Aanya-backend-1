6465e2adacb8ed6116d8298111cad2e9ee0d6190
MittalAanya <aanyaammittal@gmail.com>
2022-11-06 14:11:14 +0530

    commitn

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser: true});
+mongoose.connect("mongodb+srv://AanyaMittal:aanya5528@cluster0.jd0gxrs.mongodb.net/test",{useNewUrlParser: true});
 const postSchema = new mongoose.Schema({
     title: String,
     content: String
